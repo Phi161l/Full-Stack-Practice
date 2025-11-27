0b2ee655602b7de1fe7eb9fa61a9526375b2991e
Phi161l <ibrahimnaik91@gmail.com>
2025-11-26 18:58:50 -0800

    renaming folder name

--- 4. simple Nextjs/mini-next-shop/app/about/page.js
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-  return (
-    <div>
-      <h2>About Mini Next Shop</h2>
-      <p>This is a small Next.js project to learn routing, layouts, and API routes.</p>
-    </div>
-  );
-}
