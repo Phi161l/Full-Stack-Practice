93ab7a1359ecac58c002172a44e635ccb522c010
ibraX <ibrahimnaik91@gmail.com>
2025-12-10 06:21:17 -0800

    added the backen api,  the GET,POST,PUT,DELETE route handlers

---
+++ frontend/7. notes-app/src/app/api/notes/[id]/route.js
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+
+const filePath = path.join(process.cwd(), "src/data/notes.json");
+
+// UPDATE note
+export async function PUT(request, { params }) {
+  const { id: strid } = await params;  
+  const id = Number(strid);    
+
+  const body = await request.json();
+
+  const data = fs.readFileSync(filePath, "utf-8");
+  const notes = JSON.parse(data);
+
+  const index = notes.findIndex((n) => n.id === id);
+
+  if (index === -1) {
+    return NextResponse.json({ error: "Note not found" }, { status: 404 });
+  }
+
+  notes[index].title = body.title;
+  notes[index].content = body.content;
+
+  fs.writeFileSync(filePath, JSON.stringify(notes, null, 2));
+
+  return NextResponse.json(notes[index]);
+}
+
+
+// DELETE note
+export async function DELETE(request, { params }) {
+  const { id: strid } = await params;  
+  const id = Number(strid);    
+
+  
+  const data = fs.readFileSync(filePath, "utf-8");
+  let notes = JSON.parse(data);
+
+  const filtered = notes.filter((n) => n.id !== id);
+
+  fs.writeFileSync(filePath, JSON.stringify(filtered, null, 2));
+
+  return NextResponse.json({ message: "Deleted successfully" });
+}
---
+++ frontend/7. notes-app/src/app/api/notes/route.js
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+
+
+const filePath = path.join(process.cwd(), "src/data/notes.json");
+
+// GET all notes
+export async function GET() {
+  const data = await fs.readFileSync(filePath, "utf-8");
+
+  const notes = JSON.parse(data);
+  return NextResponse.json(notes);
+
+}
+
+// Post for new note
+export async function POST(req) {
+  const body = await req.json();
+
+  const data = await fs.readFileSync(filePath, "utf-8");
+  const notes = JSON.parse(data);
+
+  const newNote = {
+    id: Date.now(),
+    title: body.title,
+    content: body.content
+  }
+
+  notes.push(newNote);
+
+  fs.writeFileSync(filePath, JSON.stringify(notes, null, 2));
+
+  return NextResponse.json(newNote, {status: 201});
+
+}
+
