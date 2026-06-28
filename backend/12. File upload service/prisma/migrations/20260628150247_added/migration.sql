4515bec01b39223cd9029582aaee00cfa7c23194
ibraX <ibrahimnaik91@gmail.com>
2026-06-28 18:05:46 +0300

    file-upload-service: feat: store uploaded file metadata in database using prisma

---
+++ backend/12. File upload service/prisma/migrations/20260628150247_added/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" SERIAL NOT NULL,
+    "filename" TEXT NOT NULL,
+    "originalName" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "mimeType" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
--- backend/12. File upload service/src/controllers/upload.controller.ts
+++ backend/12. File upload service/src/controllers/upload.controller.ts
@@ -1,19 +1,26 @@
 import { Request, Response } from "express";
+import {prisma} from "../lib/prisma";
 
-export const uploadFile = (req: Request, res: Response) => {
+export const uploadFile = async (req: Request, res: Response) => {
   const file = req.file;
 
   if (!file) {
-    return res.status(400).json({ message: "No file uploaded" });
+    return res.status(400).json({
+      message: "No file uploaded",
+    });
   }
 
-  res.json({
-    message: "File uploaded successfully",
-    file: {
+  const savedFile = await prisma.file.create({
+    data: {
       filename: file.filename,
       originalName: file.originalname,
-      size: file.size,
+
       path: file.path,
+      size: file.size,
+
+      mimeType: file.mimetype,
     },
   });
+
+  res.status(201).json(savedFile);
 };
